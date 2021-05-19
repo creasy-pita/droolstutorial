5069460ce97fe3949978f4fadd31375b74f96ca7
creasy-pita <hz_ljq@live.com>
2021-05-19 09:50:17 +0800

    1 增加drools运行时单元测试

--- DroolsTest/src/main/java/com/Runtime/RuntimeTest.java
+++ DroolsTest/src/main/java/com/Runtime/RuntimeTest.java
@@ -5,26 +5,31 @@ import org.kie.api.definition.rule.Rule;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Created by lujq on 5/13/2021.
  */
 public class RuntimeTest {
-    public RuleProvider localRuleProvider() {
-        return new LocalFileRuleProvider("rules/runtime/zwzt.drl");
+//    public RuleProvider localRuleProvider() {
+//        return new LocalFileRuleProvider("rules/runtime/zwzt.drl");
+//    }
+
+    public Map<String,RuleProvider> localRuleProvider() {
+        Map<String, RuleProvider> rpMap = new HashMap<>();
+        rpMap.put("zwzt", new LocalFileRuleProvider("rules/runtime/zwzt.drl"));
+        rpMap.put("zbpt", new LocalFileRuleProvider("rules/runtime/zbpt.drl"));
+        return rpMap;
     }
 
     @Test
     public void DroolsRuntimeTest() throws Exception {
-
-        GisRuleEngineRuntimeManager runtimeManager = new GisRuleEngineRuntimeManager(localRuleProvider());
+        GisRuleEngineRuntimeManager runtimeManager = new GisRuleEngineRuntimeManager();
+        runtimeManager.putRuntime("zwzt", new LocalFileRuleProvider("rules/runtime/zwzt.drl"));
+        String projectCode = "zwzt";
         // 初始化drools
-        KieContainer kieContainer = runtimeManager.getDefaultRuntime().getKieContainer();
+        KieContainer kieContainer = runtimeManager.getRuntime(projectCode).getKieContainer();
         KieSession kieSession = kieContainer.newKieSession();
         // 设置Filter
         GisAgendaFilter filter = new GisAgendaFilter(Collections.singletonList("申请人姓名不为空"));
@@ -38,7 +43,72 @@ public class RuntimeTest {
         kieSession.dispose();
         List<String> firedRuleNameList = listener.getFiredRuleNameList();
         System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
         System.out.println(firedRuleNameList);
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        runtimeManager.getRuntime(projectCode).clearRule();
+
+        runtimeManager.putRuntime("zbpt", new LocalFileRuleProvider("rules/runtime/zbpt.drl"));
+        projectCode = "zbpt";
+        // 初始化drools
+        kieContainer = runtimeManager.getRuntime(projectCode).getKieContainer();
+        kieSession = kieContainer.newKieSession();
+        // 设置Filter
+        filter = new GisAgendaFilter(Collections.singletonList("申请人姓名不为空"));
+        // eventListener
+        listener = new GisAgendaEventListener();
+        kieSession.addEventListener(listener);
+        //传递需要执行的规则名称列表
+        //kieSession.insert(jsonObject);
+//        kieSession.setGlobal("$obj", jsonObject);
+        kieSession.fireAllRules(filter);
+        kieSession.dispose();
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        System.out.println(listener.getFiredRuleNameList());
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+
+        projectCode = "zwzt";
+        // 初始化drools
+        kieContainer = runtimeManager.getRuntime(projectCode).getKieContainer();
+        kieSession = kieContainer.newKieSession();
+        // 设置Filter
+        filter = new GisAgendaFilter(Collections.singletonList("申请人姓名不为空"));
+        // eventListener
+        listener = new GisAgendaEventListener();
+        kieSession.addEventListener(listener);
+        //传递需要执行的规则名称列表
+        //kieSession.insert(jsonObject);
+//        kieSession.setGlobal("$obj", jsonObject);
+        kieSession.fireAllRules(filter);
+        kieSession.dispose();
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        System.out.println(listener.getFiredRuleNameList());
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+
+        projectCode = "zbpt";
+        // 初始化drools
+        kieContainer = runtimeManager.getRuntime(projectCode).getKieContainer();
+        kieSession = kieContainer.newKieSession();
+        // 设置Filter
+        filter = new GisAgendaFilter(Collections.singletonList("申请人姓名不为空"));
+        // eventListener
+        listener = new GisAgendaEventListener();
+        kieSession.addEventListener(listener);
+        //传递需要执行的规则名称列表
+        //kieSession.insert(jsonObject);
+//        kieSession.setGlobal("$obj", jsonObject);
+        kieSession.fireAllRules(filter);
+        kieSession.dispose();
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        System.out.println(listener.getFiredRuleNameList());
+        System.out.println("---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
 
     }
 
